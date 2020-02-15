7335dd7463f77a8df854e76d0f5d1612666d7f52
Jake Hemstad <jhemstad@nvidia.com>
2020-02-15 10:02:11 -0600

    Stub catch test file.

--- tests/insert_only_hash_array/insert_only_hash_array_test.cu
+++ tests/insert_only_hash_array/insert_only_hash_array_test.cu
@@ -14,21 +14,30 @@
  * limitations under the License.
  */
 
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include <catch.hpp>
+
+unsigned int Factorial( unsigned int number ) {
+    return number <= 1 ? number : Factorial(number-1)*number;
+}
+
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}
+
+/*
 #include <insert_only_hash_array.cuh>
-
-#include <gmock/gmock.h>
-#include <gtest/gtest.h>
 #include <cstdint>
-
 #include <thrust/for_each.h>
 #include <simt/atomic>
+*/
 
 
-struct inserter{
-
-
-};
-
+/*
 TEST(FirstTest, First) {
   insert_only_hash_array<int32_t, int32_t> a{1000, -1, -1};
 
@@ -45,3 +54,4 @@ TEST(FirstTest, First) {
 
   (void)view;
 }
+*/
