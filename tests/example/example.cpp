e5bf8aff4a3b32e2a24c35f43f6125dc95f5edf1
Jake Hemstad <jhemstad@nvidia.com>
2020-02-15 14:20:14 -0600

    Add a catch_main.

--- tests/example/example.cpp
+++ tests/example/example.cpp
@@ -1,5 +1,4 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "catch.hpp"
+#include <catch.hpp>
 
 unsigned int Factorial( unsigned int number ) {
     return number <= 1 ? number : Factorial(number-1)*number;
--- tests/example/example.cu
+++ tests/example/example.cu
@@ -1,6 +1,4 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do
-                           // this in one cpp file
-#include "catch.hpp"
+#include <catch.hpp>
 
 #include <thrust/device_vector.h>
 #include <thrust/reduce.h>
