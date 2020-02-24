9dbad83033c2063e163aae2c155311416b9916ca
Jake Hemstad <jhemstad@nvidia.com>
2020-02-24 10:52:57 -0600

    In progress testing. Using kernel instead of Thrust.

--- tests/insert_only_hash_array/insert_only_hash_array_test.cu
+++ tests/insert_only_hash_array/insert_only_hash_array_test.cu
@@ -14,38 +14,46 @@
  * limitations under the License.
  */
 
+#include <cu_collections/utilities/error.hpp>
+#include <insert_only_hash_array.cuh>
+
+#include <thrust/device_vector.h>
+#include <thrust/for_each.h>
+#include <algorithm>
 #include <catch.hpp>
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
+template <typename Map, typename Pairs>
+__global__ void insert_kernel(Map map, Pairs const* pairs, size_t size) {
+  auto tid = threadIdx.x + blockIdx.x * blockDim.x;
+  if (tid < size) map.insert(pairs[tid]);
 }
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
-}
-
-/*
-*/
-
-
-/*
-TEST(FirstTest, First) {
+TEST_CASE("The first test") {
   insert_only_hash_array<int32_t, int32_t> a{1000, -1, -1};
+
+  CUDA_TRY(cudaDeviceSynchronize());
 
   auto view = a.get_device_view();
 
   std::vector<thrust::pair<int32_t, int32_t>> pairs(100);
   std::generate(pairs.begin(), pairs.end(), []() {
     static int32_t counter{};
-    return thrust::make_pair(++counter, counter);
+    ++counter;
+    return thrust::make_pair(counter, counter);
   });
 
   thrust::device_vector<thrust::pair<int32_t, int32_t>> d_pairs(pairs);
 
+  CUDA_TRY(cudaDeviceSynchronize());
 
-  (void)view;
+  // thrust::for_each(
+  //    d_pairs.begin(), d_pairs.end(),
+  //    [view] __device__(
+  //        thrust::pair<const int32_t, int32_t> const& pair) mutable {
+  //      view.insert(pair);
+  //    });
+
+  insert_kernel<<<5, 256>>>(view, d_pairs.data().get(), d_pairs.size());
+
+  CUDA_TRY(cudaDeviceSynchronize());
 }
-*/
