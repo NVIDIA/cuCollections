a68936de389ec35eb06f606832a40a158e2b6801
Jake Hemstad <jhemstad@nvidia.com>
2020-07-02 13:19:10 -0500

    Disable non-working RBK for cuco.

--- benchmarks/reduce_by_key/reduce_by_key.cu
+++ benchmarks/reduce_by_key/reduce_by_key.cu
@@ -16,6 +16,7 @@
 
 #include <benchmark/benchmark.h>
 #include <thrust/iterator/iterator_traits.h>
+#include <thrust/iterator/zip_iterator.h>
 #include <thrust/pair.h>
 #include <thrust/reduce.h>
 #include <thrust/sort.h>
@@ -33,7 +34,9 @@
 static void generate_size_and_num_unique(benchmark::internal::Benchmark* b)
 {
   for (auto num_unique = 64; num_unique <= 1 << 20; num_unique <<= 1) {
-    for (auto size = 10'000'000; size <= 10'000'000; size *= 10) { b->Args({size, num_unique}); }
+    for (auto size = 10'000'000; size <= 10'000'000; size *= 10) {
+      b->Args({size, num_unique});
+    }
   }
 }
 
@@ -204,6 +207,7 @@ BENCHMARK_TEMPLATE(BM_cudf, unsigned long long int, unsigned long long int)
   ->Unit(benchmark::kMillisecond)
   ->Apply(generate_size_and_num_unique);
 
+/*
 template <typename KeyRandomIterator, typename ValueRandomIterator>
 void cuco_cas_reduce_by_key(KeyRandomIterator keys_begin,
                             KeyRandomIterator keys_end,
@@ -219,15 +223,20 @@ void cuco_cas_reduce_by_key(KeyRandomIterator keys_begin,
 
   using map_type = cuco::insert_only_hash_array<Key, Value, cuda::thread_scope_device>;
 
-  map_type map{map_capacity, -1};
+  map_type map{map_capacity, -1, -1};
 
-  thrust::transform(
-    thrust::device,
-    keys_begin,
-    keys_end,
-    values_begin,
-    thrust::make_discard_iterator(),
-    [view = map.get_device_view()] __device__(Key const& k, Value const& v) mutable {
+  auto zipped_begin = thrust::make_zip_iterator(thrust::make_tuple(keys_begin, values_begin));
+  auto zipped_end   = zipped_begin + thrust::distance(keys_begin, keys_end);
+
+  std::cout << "hello\n";
+
+  thrust::for_each(thrust::device,
+                   zipped_begin,
+                   zipped_end,
+                   [view = map.get_device_view()] __device__(auto const& key_value) mutable {
+                     auto k = thrust::get<0>(key_value);
+                     auto v = thrust::get<1>(key_value);
+
       auto found = view.find(k);
 
       if (view.end() == found) {
@@ -235,16 +244,9 @@ void cuco_cas_reduce_by_key(KeyRandomIterator keys_begin,
         found       = result.first;
       }
 
-      /*
-              auto expected = found->load(cuda::std::memory_order_relaxed);
+                     // TODO: JH: This is currently causing a hang for some reason.
 
-              while ( not found->compare_exchange_weak(
-                      expected, cuco::make_pair(expected.first, expected.second + v)),
-                  cuda::std::memory_order_relaxed) {
-              }
-              */
-
-      return 0;
+                     found->second.fetch_add(v, cuda::std::memory_order_relaxed);
     });
 }
 
@@ -259,7 +261,7 @@ static void BM_cuco_cas(::benchmark::State& state)
   for (auto _ : state) {
     state.PauseTiming();
     thrust::device_vector<Key> keys(state.range(0));
-    auto begin = thrust::make_counting_iterator(0);
+    auto begin = thrust::make_counting_iterator(1);
     thrust::transform(
       begin, begin + state.range(0), keys.begin(), [num_unique_keys] __device__(auto i) {
         return i % num_unique_keys;
