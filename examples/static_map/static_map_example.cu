09cb94915b9161ebfe39ff572ad8f14d7c3e70b9
Jake Hemstad <jhemstad@nvidia.com>
2020-09-21 10:23:55 -0500

    Update example to do a bulk find too.

--- examples/static_map/static_map_example.cu
+++ examples/static_map/static_map_example.cu
@@ -14,17 +14,43 @@
  * limitations under the License.
  */
 
-#include <thrust/device_vector.h>
 #include <limits>
+
+#include <thrust/device_vector.h>
+#include <thrust/sequence.h>
+#include <thrust/transform.h>
 
 #include <cuco/static_map.cuh>
 
 int main(void)
 {
-  int empty_key_sentinel   = std::numeric_limits<int>::max();
-  int empty_value_sentinel = std::numeric_limits<int>::max();
-  cuco::static_map<int, int> my_map{100'000, empty_key_sentinel, empty_value_sentinel};
+  int empty_key_sentinel   = -1;
+  int empty_value_sentinel = -1;
+
+  // Constructs a map with 100,000 slots using -1 and -1 as the empty key/value
+  // sentinels. Note the capacity is chosen knowing we will insert 50,000 keys,
+  // for an load factor of 50%.
+  cuco::static_map<int, int> map{100'000, empty_key_sentinel, empty_value_sentinel};
+
   thrust::device_vector<thrust::pair<int, int>> pairs(50'000);
-  my_map.insert(pairs.begin(), pairs.end());
+
+  // Create a sequence of pairs {{0,0}, {1,1}, ... {i,i}}
+  thrust::transform(thrust::make_counting_iterator<int>(0),
+                    thrust::make_counting_iterator<int>(pairs.size()),
+                    pairs.begin(),
+                    [] __device__(auto i) { return thrust::make_pair(i, i); });
+
+  // Inserts all pairs into the map
+  map.insert(pairs.begin(), pairs.end());
+
+  // Sequence of keys {0, 1, 2, ...}
+  thrust::device_vector<int> keys_to_find(50'000);
+  thrust::sequence(keys_to_find.begin(), keys_to_find.end(), 0);
+  thrust::device_vector<int> found_values(50'000);
+
+  // Finds all keys {0, 1, 2, ...} and stores associated values into `found_values`
+  // If a key `keys_to_find[i]` doesn't exist, `found_values[i] == empty_value_sentinel`
+  map.find(keys_to_find.begin(), keys_to_find.end(), found_values.begin());
+
   return 0;
 }
