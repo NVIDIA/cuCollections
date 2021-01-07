24261b2a9b4e327409a26869804dea8abc78a993
Jake Hemstad <jhemstad@nvidia.com>
2021-01-07 11:55:12 -0600

    Add test for inserting all unique keys.

--- tests/static_reduction_map/static_reduction_map_test.cu
+++ tests/static_reduction_map/static_reduction_map_test.cu
@@ -23,8 +23,6 @@
 #include <limits>
 
 namespace {
-namespace cg = cooperative_groups;
-
 // Thrust logical algorithms (any_of/all_of/none_of) don't work with device
 // lambdas: See https://github.com/thrust/thrust/issues/1062
 template <typename Iterator, typename Predicate>
@@ -47,74 +45,73 @@ bool none_of(Iterator begin, Iterator end, Predicate p)
 }
 }  // namespace
 
-enum class dist_type { UNIQUE, UNIFORM, GAUSSIAN };
-
-template <dist_type Dist, typename Key, typename OutputIt>
-static void generate_keys(OutputIt output_begin, OutputIt output_end)
-{
-  auto num_keys = std::distance(output_begin, output_end);
-
-  std::random_device rd;
-  std::mt19937 gen{rd()};
-
-  switch (Dist) {
-    case dist_type::UNIQUE:
-      for (auto i = 0; i < num_keys; ++i) {
-        output_begin[i] = i;
-      }
-      break;
-    case dist_type::UNIFORM:
-      for (auto i = 0; i < num_keys; ++i) {
-        output_begin[i] = std::abs(static_cast<Key>(gen()));
-      }
-      break;
-    case dist_type::GAUSSIAN:
-      std::normal_distribution<> dg{1e9, 1e7};
-      for (auto i = 0; i < num_keys; ++i) {
-        output_begin[i] = std::abs(static_cast<Key>(dg(gen)));
-      }
-      break;
-  }
-}
-
-TEMPLATE_TEST_CASE_SIG("Unique sequence of keys",
+TEMPLATE_TEST_CASE_SIG("Insert all identical keys",
                        "",
-                       ((typename T, dist_type Dist), T, Dist),
-                       (int32_t, dist_type::UNIQUE))
+                       ((typename Key, typename Value), Key, Value),
+                       (int32_t, int32_t))
 {
-  using Key   = T;
-  using Value = T;
-
   constexpr std::size_t num_slots{200};
   cuco::static_reduction_map<cuco::reduce_add<Value>, Key, Value> map{num_slots, -1};
 
-  SECTION("Inserting identical keys")
+  thrust::device_vector<Key> keys(100, 42);
+  thrust::device_vector<Value> values(keys.size(), 1);
+  auto zip     = thrust::make_zip_iterator(thrust::make_tuple(keys.begin(), values.begin()));
+  auto zip_end = zip + keys.size();
+  map.insert(zip, zip_end);
+
+  SECTION("There should only be one key in the map") { REQUIRE(map.get_size() == 1); }
+
+  SECTION("Map should contain the inserted key")
   {
-    thrust::device_vector<Key> keys(100, 42);
-    thrust::device_vector<Value> values(keys.size(), 1);
-    auto zip     = thrust::make_zip_iterator(thrust::make_tuple(keys.begin(), values.begin()));
-    auto zip_end = zip + keys.size();
-    map.insert(zip, zip_end);
+    thrust::device_vector<bool> contained(keys.size());
+    map.contains(keys.begin(), keys.end(), contained.begin());
+    REQUIRE(all_of(contained.begin(), contained.end(), [] __device__(bool c) { return c; }));
+  }
 
-    SECTION("There should only be one key in the map") { REQUIRE(map.get_size() == 1); }
+  SECTION("Found value should equal aggregate of inserted values")
+  {
+    thrust::device_vector<Value> found(keys.size());
+    map.find(keys.begin(), keys.end(), found.begin());
+    auto const expected_aggregate = keys.size();  // All keys inserted "1", so the
+                                                  // sum aggregate should be
+                                                  // equal to the number of keys inserted
+    REQUIRE(all_of(found.begin(), found.end(), [expected_aggregate] __device__(Value v) {
+      return v == expected_aggregate;
+    }));
+  }
+}
 
-    SECTION("Map should contain the inserted key")
-    {
-      thrust::device_vector<bool> contained(keys.size());
-      map.contains(keys.begin(), keys.end(), contained.begin());
-      REQUIRE(all_of(contained.begin(), contained.end(), [] __device__(bool c) { return c; }));
-    }
+TEMPLATE_TEST_CASE_SIG("Insert all unique keys",
+                       "",
+                       ((typename Key, typename Value), Key, Value),
+                       (int32_t, int32_t))
+{
+  constexpr std::size_t num_keys = 100;
+  constexpr std::size_t num_slots{num_keys * 3};
+  cuco::static_reduction_map<cuco::reduce_add<Value>, Key, Value> map{num_slots, -1};
 
-    SECTION("Found value should equal aggregate of inserted values")
-    {
-      thrust::device_vector<Value> found(keys.size());
-      map.find(keys.begin(), keys.end(), found.begin());
-      auto const expected_aggregate = keys.size();  // All keys inserted "1", so the
-                                                    // sum aggregate should be
-                                                    // equal to the number of keys inserted
-      REQUIRE(all_of(found.begin(), found.end(), [expected_aggregate] __device__(Value v) {
-        return v == expected_aggregate;
-      }));
-    }
+  auto keys_begin   = thrust::make_counting_iterator<Key>(0);
+  auto values_begin = thrust::make_counting_iterator<Value>(0);
+  auto zip          = thrust::make_zip_iterator(thrust::make_tuple(keys_begin, values_begin));
+  auto zip_end      = zip + num_keys;
+  map.insert(zip, zip_end);
+
+  SECTION("Size of map should equal number of inserted keys")
+  {
+    REQUIRE(map.get_size() == num_keys);
+  }
+
+  SECTION("Map should contain the inserted keys")
+  {
+    thrust::device_vector<bool> contained(num_keys);
+    map.contains(keys_begin, keys_begin + num_keys, contained.begin());
+    REQUIRE(all_of(contained.begin(), contained.end(), [] __device__(bool c) { return c; }));
+  }
+
+  SECTION("Found value should equal inserted value")
+  {
+    thrust::device_vector<Value> found(num_keys);
+    map.find(keys_begin, keys_begin + num_keys, found.begin());
+    REQUIRE(thrust::equal(thrust::device, values_begin, values_begin + num_keys, found.begin()));
   }
 }
