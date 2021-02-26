c6a7917bcda07b5764374de277b597febaa58721
Yunsong Wang <yunsongw@nvidia.com>
2021-02-26 13:55:06 -0800

    Add static_multimap tests

---
+++ tests/static_multimap/static_multimap_test.cu
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2021, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <thrust/count.h>
+#include <thrust/device_vector.h>
+#include <thrust/for_each.h>
+#include <algorithm>
+#include <catch2/catch.hpp>
+#include <cuco/static_multimap.cuh>
+#include <limits>
+
+namespace {
+namespace cg = cooperative_groups;
+
+// Thrust logical algorithms (any_of/all_of/none_of) don't work with device
+// lambdas: See https://github.com/thrust/thrust/issues/1062
+template <typename Iterator, typename Predicate>
+bool all_of(Iterator begin, Iterator end, Predicate p)
+{
+  auto size = thrust::distance(begin, end);
+  return size == thrust::count_if(begin, end, p);
+}
+
+template <typename Iterator, typename Predicate>
+bool any_of(Iterator begin, Iterator end, Predicate p)
+{
+  return thrust::count_if(begin, end, p) > 0;
+}
+
+template <typename Iterator, typename Predicate>
+bool none_of(Iterator begin, Iterator end, Predicate p)
+{
+  return not all_of(begin, end, p);
+}
+}  // namespace
+
+enum class dist_type { UNIQUE, UNIFORM, GAUSSIAN };
+
+template <dist_type Dist, typename Key, typename OutputIt>
+static void generate_keys(OutputIt output_begin, OutputIt output_end)
+{
+  auto num_keys = std::distance(output_begin, output_end);
+
+  std::random_device rd;
+  std::mt19937 gen{rd()};
+
+  switch (Dist) {
+    case dist_type::UNIQUE:
+      for (auto i = 0; i < num_keys; ++i) {
+        output_begin[i] = i;
+      }
+      break;
+    case dist_type::UNIFORM:
+      for (auto i = 0; i < num_keys; ++i) {
+        output_begin[i] = std::abs(static_cast<Key>(gen()));
+      }
+      break;
+    case dist_type::GAUSSIAN:
+      std::normal_distribution<> dg{1e9, 1e7};
+      for (auto i = 0; i < num_keys; ++i) {
+        output_begin[i] = std::abs(static_cast<Key>(dg(gen)));
+      }
+      break;
+  }
+}
+
+TEMPLATE_TEST_CASE_SIG("Unique sequence of keys",
+                       "",
+                       ((typename T, dist_type Dist), T, Dist),
+                       (int32_t, dist_type::UNIQUE),
+                       (int64_t, dist_type::UNIQUE),
+                       (int32_t, dist_type::UNIFORM),
+                       (int64_t, dist_type::UNIFORM),
+                       (int32_t, dist_type::GAUSSIAN),
+                       (int64_t, dist_type::GAUSSIAN))
+{
+  using Key   = T;
+  using Value = T;
+
+  constexpr std::size_t num_keys{50'000'000};
+  cuco::static_multimap<Key, Value> map{100'000'000, -1, -1};
+
+  auto m_view = map.get_device_mutable_view();
+  auto view   = map.get_device_view();
+
+  std::vector<Key> h_keys(num_keys);
+  std::vector<Value> h_values(num_keys);
+  std::vector<cuco::pair_type<Key, Value>> h_pairs(num_keys);
+
+  generate_keys<Dist, Key>(h_keys.begin(), h_keys.end());
+
+  for (auto i = 0; i < num_keys; ++i) {
+    Key key           = h_keys[i];
+    Value val         = h_keys[i];
+    h_pairs[i].first  = key;
+    h_pairs[i].second = val;
+    h_values[i]       = val;
+  }
+
+  thrust::device_vector<Key> d_keys(h_keys);
+  thrust::device_vector<Value> d_values(h_values);
+  thrust::device_vector<cuco::pair_type<Key, Value>> d_pairs(h_pairs);
+  thrust::device_vector<Value> d_results(num_keys);
+  thrust::device_vector<bool> d_contained(num_keys);
+
+  // bulk function test cases
+  SECTION("All inserted keys-value pairs should be correctly recovered during find")
+  {
+    map.insert(d_pairs.begin(), d_pairs.end());
+    map.find(d_keys.begin(), d_keys.end(), d_results.begin());
+    auto zip = thrust::make_zip_iterator(thrust::make_tuple(d_results.begin(), d_values.begin()));
+
+    REQUIRE(all_of(zip, zip + num_keys, [] __device__(auto const& p) {
+      return thrust::get<0>(p) == thrust::get<1>(p);
+    }));
+  }
+}
