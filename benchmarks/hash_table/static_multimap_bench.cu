4303e2632f2137c6c5d92fd50e4698abc3eb46b2
Yunsong Wang <yunsongw@nvidia.com>
2021-03-15 17:09:29 -0700

    Update static multimap benchmark

--- benchmarks/hash_table/static_multimap_bench.cu
+++ benchmarks/hash_table/static_multimap_bench.cu
@@ -105,8 +105,19 @@ void nvbench_static_multimap_insert(nvbench::state& state, nvbench::type_list<Ke
 
 using key_type   = nvbench::type_list<nvbench::int32_t, nvbench::int64_t>;
 using value_type = nvbench::type_list<nvbench::int32_t, nvbench::int64_t>;
+
 NVBENCH_BENCH_TYPES(nvbench_static_multimap_insert, NVBENCH_TYPE_AXES(key_type, value_type))
   .set_type_axes_names({"Key", "Value"})
-  .add_int64_axis("NumInputs", {100'000'000})
-  .add_float64_axis("Occupancy", nvbench::range(0.1, 0.9, 0.1))
+  .set_timeout(100)                        // Custom timeout: 100 s. By default: 15 s.
+  .set_max_noise(3)                        // Custom timeout: 3%. By default: 0.5%.
+  .add_int64_axis("NumInputs", {2 << 27})  // Total number of key/value pairs: 2^28
+  .add_float64_axis("Occupancy", {0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95})
+  .add_string_axis("Distribution", {"UNIQUE", "UNIFORM", "GAUSSIAN"});
+
+NVBENCH_BENCH_TYPES(nvbench_static_multimap_insert, NVBENCH_TYPE_AXES(key_type, value_type))
+  .set_type_axes_names({"Key", "Value"})
+  .set_timeout(1'500)                      // Custom timeout: 1500 s. By default: 15 s.
+  .set_max_noise(3)                        // Custom timeout: 3%. By default: 0.5%.
+  .add_int64_axis("NumInputs", {2 << 27})  // Total number of key/value pairs: 2^28
+  .add_float64_axis("Occupancy", {1.})
   .add_string_axis("Distribution", {"UNIQUE", "UNIFORM", "GAUSSIAN"});
