3889d1950def5ab977400e29acce46b4b5af337d
Yunsong Wang <yunsongw@nvidia.com>
2021-03-24 15:57:52 -0400

    Update notebook: add multimap CG size analysis + update comments

--- benchmarks/analysis/notebooks/Utils.py
+++ benchmarks/analysis/notebooks/Utils.py
@@ -7,20 +7,25 @@ import matplotlib
 colors = ['b','r','g','m','y','c']
 styles = ['o','s','v','^','D',">"]
 
-def plot_insert(bm, df, xaxis, unique_labels, flag = False):
+def plot_perf(bm, df, xaxis, unique_labels, is_multivalue = False, is_singletype = False):
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
     fig.suptitle(bm)
     
     marker_handles = []
     
+    # Frist sub-figure shows performance (#ops/s)
     ax1.set_xlabel(xaxis)
     ax1.set_ylabel('Performance (number of operations per second)')
 
+    # Second sub-figure shows bandwidth
     ax2.set_xlabel(xaxis)
     ax2.set_ylabel('Bandwidth (GB/s)')
     
+    # Custom x-axis for multi-value test cases:
+    # - power-of-two ticks
+    # - log-scale
     lax = [ax1, ax2]
-    if flag:
+    if is_multivalue:
         lnum = list(df[xaxis])
         
         for item in lax:
@@ -28,8 +33,12 @@ def plot_insert(bm, df, xaxis, unique_labels, flag = False):
             item.set_xticks(lnum)
             item.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
         
-    num_dist = len(df["Distribution"].unique())
+    if not is_singletype:
+        num_style = len(df["Distribution"].unique())
+    else:
+        num_style = len(df["CGSize"].unique())
 
+    # Iterate over labels and label indices
     for lindex, lbl in enumerate(unique_labels):
         tmpdf = df.loc[df['Label'] == lbl]
 
@@ -37,29 +46,29 @@ def plot_insert(bm, df, xaxis, unique_labels, flag = False):
         perf = tmpdf["Elem/s (elem/sec)"]
         bw   = tmpdf['Bandwidth (GB/s)']
 
-        # Get distribution & type index
-        did = lindex % num_dist
-        tid = int(lindex / num_dist)
+        # Get style & type index
+        sid = lindex % num_style
+        tid = int(lindex / num_style)
 
-        if not tid:
-            ax1.plot(x, perf, color=colors[did])
-            ax1.scatter(x, perf, color=colors[did], marker=styles[did])
+        if (not tid) or is_singletype:
+            ax1.plot(x, perf, color=colors[sid])
+            ax1.scatter(x, perf, color=colors[sid], marker=styles[sid])
 
-            ax2.plot(x, bw, color=colors[did])
-            ax2.scatter(x, bw, color=colors[did], marker=styles[did])
+            ax2.plot(x, bw, color=colors[sid])
+            ax2.scatter(x, bw, color=colors[sid], marker=styles[sid])
 
             # Add legend
-            marker_handles.append(ax1.plot([], [], c=colors[did], marker=styles[did], \
+            marker_handles.append(ax1.plot([], [], c=colors[sid], marker=styles[sid], \
                                           label=lbl)[0])
         else:
-            ax1.plot(x, perf, color=colors[did], linestyle="--")
-            ax1.scatter(x, perf, color=colors[did], marker=styles[did], facecolors='none')
+            ax1.plot(x, perf, color=colors[sid], linestyle="--")
+            ax1.scatter(x, perf, color=colors[sid], marker=styles[sid], facecolors='none')
 
-            ax2.plot(x, bw, color=colors[did], linestyle="--")
-            ax2.scatter(x, bw, color=colors[did], marker=styles[did], facecolors='none')
+            ax2.plot(x, bw, color=colors[sid], linestyle="--")
+            ax2.scatter(x, bw, color=colors[sid], marker=styles[sid], facecolors='none')
 
             # Add legend
-            marker_handles.append(ax1.plot([], [], c=colors[did], marker=styles[did], \
+            marker_handles.append(ax1.plot([], [], c=colors[sid], marker=styles[sid], \
                                           mfc='none', linestyle="--", label=lbl)[0])
     
     leg = plt.legend(handles = marker_handles, loc="lower left", ncol=2, frameon=False)
