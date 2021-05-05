db5bae0573be9c7fcef65bb493f1b559e118735d
Jake Hemstad <jhemstad@nvidia.com>
2021-05-05 08:24:51 -0500

    Improve existing example and rename.

---
+++ examples/static_map/host_bulk_example.cu
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2020, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <limits>
+#include <iostream>
+
+#include <thrust/device_vector.h>
+#include <thrust/sequence.h>
+#include <thrust/transform.h>
+#include <thrust/equal.h>
+#include <thrust/iterator/zip_iterator.h>
+
+#include <cuco/static_map.cuh>
+
+int main(void)
+{
+  using Key   = int;
+  using Value = int;
+
+  // Empty slots are represented by reserved "sentinel" values. These values should be selected such
+  // that they never occur in your input data.
+  Key const empty_key_sentinel   = -1;
+  Value const empty_value_sentinel = -1;
+
+  // Number of key/value pairs to be inserted
+  std::size_t num_keys = 50'000;
+
+  // Compute capacity based on a 50% load factor
+  auto const load_factor = 0.5;
+  std::size_t const capacity = std::ceil(num_keys/load_factor);
+
+  // Constructs a map with "capacity" slots using -1 and -1 as the empty key/value sentinels.
+  cuco::static_map<Key, Value> map{capacity, empty_key_sentinel, empty_value_sentinel};
+
+  // Create a sequence of keys and values {{0,0}, {1,1}, ... {i,i}}
+  thrust::device_vector<Key> insert_keys(num_keys);
+  thrust::sequence(insert_keys.begin(), insert_keys.end(), 0);
+  thrust::device_vector<Value> insert_values(num_keys);
+  thrust::sequence(insert_values.begin(), insert_values.end(), 0);
+  auto zipped = thrust::make_zip_iterator(thrust::make_tuple(insert_keys.begin(), insert_values.begin()));
+
+  // Inserts all pairs into the map
+  map.insert(zipped, zipped + insert_keys.size());
+
+  // Storage for found values
+  thrust::device_vector<Value> found_values(num_keys);
+
+  // Finds all keys {0, 1, 2, ...} and stores associated values into `found_values`
+  // If a key `keys_to_find[i]` doesn't exist, `found_values[i] == empty_value_sentinel`
+  map.find(insert_keys.begin(), insert_keys.end(), found_values.begin());
+
+  // Verify that all the found values match the inserted values
+  bool const all_values_match = thrust::equal(found_values.begin(), found_values.end(), insert_values.begin());
+
+  if (all_values_match) { std::cout << "Success! Found all values\n"; }
+
+}
