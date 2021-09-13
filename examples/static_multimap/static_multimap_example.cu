4174391d19091cd475ead0f8b0724668cdc0cfcb
Yunsong Wang <yunsongw@nvidia.com>
2021-09-13 14:25:20 -0400

    Update multimap example: add comments, show outer use case

--- examples/static_multimap/static_multimap_example.cu
+++ examples/static_multimap/static_multimap_example.cu
@@ -27,35 +27,43 @@ int main(void)
   int empty_key_sentinel   = -1;
   int empty_value_sentinel = -1;
 
+  constexpr std::size_t N = 50'000;
+
   // Constructs a multimap with 100,000 slots using -1 and -1 as the empty key/value
   // sentinels. Note the capacity is chosen knowing we will insert 50,000 keys,
   // for an load factor of 50%.
-  cuco::static_multimap<int, int> map{100'000, empty_key_sentinel, empty_value_sentinel};
+  cuco::static_multimap<int, int> map{N * 2, empty_key_sentinel, empty_value_sentinel};
 
-  thrust::device_vector<thrust::pair<int, int>> pairs(50'000);
+  thrust::device_vector<thrust::pair<int, int>> pairs(N);
 
-  // Create a sequence of pairs. Eeach key corresponds to two pairs.
+  // Create a sequence of pairs. Eeach key has two matches.
   // E.g., {{0,0}, {1,1}, ... {0,25'000}, {1, 25'001}, ...}
   thrust::transform(thrust::make_counting_iterator<int>(0),
                     thrust::make_counting_iterator<int>(pairs.size()),
                     pairs.begin(),
-                    [] __device__(auto i) { return thrust::make_pair(i % (50'000 / 2), i); });
+                    [] __device__(auto i) { return thrust::make_pair(i % (N / 2), i); });
 
   // Inserts all pairs into the map
   map.insert(pairs.begin(), pairs.end());
 
-  // Sequence of keys {0, 1, 2, ...}
-  thrust::device_vector<int> keys_to_find(50'000);
+  // Sequence of probe keys {0, 1, 2, ... 49'999}
+  thrust::device_vector<int> keys_to_find(N);
   thrust::sequence(keys_to_find.begin(), keys_to_find.end(), 0);
 
-  // Counts number of matches for all keys {0, 1, 2, ... 50'000}
-  auto const output_size = map.count(keys_to_find.begin(), keys_to_find.end());
+  // Counts the occurrences of keys in [0, 50'000) contained in the multimap.
+  // The `_outer` suffix indicates that the occurrence of a non-match is 1.
+  auto const output_size = map.count_outer(keys_to_find.begin(), keys_to_find.end());
 
   thrust::device_vector<cuco::pair_type<int, int>> d_results(output_size);
 
   // Finds all keys {0, 1, 2, ...} and stores associated key/value pairs into `d_results`
   // If a key `keys_to_find[i]` doesn't exist, `d_results[i].second == empty_value_sentinel`
-  map.retrieve(keys_to_find.begin(), keys_to_find.end(), d_results.data().get());
+  auto output_end =
+    map.retrieve_outer(keys_to_find.begin(), keys_to_find.end(), d_results.data().get());
+  auto retrieve_size = output_end - d_results.data().get();
+
+  // The total number of outer matches should be `N + N / 2`
+  assert(not(output_size == retrieve_size == N + N / 2));
 
   return 0;
 }
