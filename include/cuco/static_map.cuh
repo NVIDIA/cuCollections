3162577261be5ffc5d1dfaf3e8a58e01a59891c5
Yunsong Wang <yunsongw@nvidia.com>
2022-04-28 09:39:38 -0400

    Add retrieve_all API

--- include/cuco/static_map.cuh
+++ include/cuco/static_map.cuh
@@ -297,6 +297,26 @@ class static_map {
             cudaStream_t stream = 0);
 
   /**
+   * @brief Retrieves all of the keys and their associated values.
+   *
+   * The order in which keys are returned is implementation defined and not guaranteed to be
+   * consistent between subsequent calls to `retrieve_all`.
+   *
+   * Behavior is undefined if the range beginning at `keys_out` or `values_out` is not large enough
+   * to contain the number of keys in the map.
+   *
+   * @tparam KeyOut Device accessible random access output iterator whose `value_type` is
+   * convertible from `key_type`.
+   * @tparam ValueOut Device accesible random access output iterator whose `value_type` is
+   * convertible from `mapped_type`.
+   * @param keys_out Beginning output iterator for keys
+   * @param values_out Beginning output iterator for values
+   * @param stream CUDA stream used for this operation
+   */
+  template <typename KeyOut, typename ValueOut>
+  void retrieve_all(KeyOut keys_out, ValueOut values_out, cudaStream_t stream = 0);
+
+  /**
    * @brief Indicates whether the keys in the range
    * `[first, last)` are contained in the map.
    *
