e069edc31651f4e3d020252f0f869b0995bade9c
Yunsong Wang <yunsongw@nvidia.com>
2022-05-04 14:47:49 -0400

    Add tests for retrieving from an empty map

--- tests/static_map/duplicate_keys_test.cu
+++ tests/static_map/duplicate_keys_test.cu
@@ -52,19 +52,23 @@ TEMPLATE_TEST_CASE_SIG("Duplicate keys",
   thrust::device_vector<Value> d_results(num_keys);
   thrust::device_vector<bool> d_contained(num_keys);
 
-  // bulk function test cases
-  SECTION("Retrieve all entries after insertion")
+  SECTION("Retrieve all entries")
   {
+    auto constexpr gold = num_keys / 2;
+    thrust::device_vector<Key> unique_keys(gold);
+    auto values_begin = thrust::make_discard_iterator();
+
+    // Retrieve all from an empty map
+    auto [empty_key_end, _] = map.retrieve_all(unique_keys.begin(), values_begin);
+    REQUIRE(std::distance(unique_keys.begin(), empty_key_end) == 0);
+
     map.insert(pairs_begin, pairs_begin + num_keys);
 
     auto const num_entries = map.get_size();
-    REQUIRE(num_entries == num_keys / 2);
-
-    thrust::device_vector<Key> unique_keys(num_entries);
-    auto values_begin = thrust::make_discard_iterator();
+    REQUIRE(num_entries == gold);
 
     auto [key_out_end, value_out_end] = map.retrieve_all(unique_keys.begin(), values_begin);
-    REQUIRE(std::distance(unique_keys.begin(), key_out_end) == map.get_size());
+    REQUIRE(std::distance(unique_keys.begin(), key_out_end) == gold);
 
     thrust::sort(thrust::device, unique_keys.begin(), unique_keys.end());
     REQUIRE(cuco::test::equal(unique_keys.begin(),
