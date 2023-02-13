02b391ba1090d15fd3d5f8638cf418d5b730ab26
Yunsong Wang <yunsongw@nvidia.com>
2023-02-13 13:31:14 -0500

    Update benchmarks with new key generators

--- benchmarks/hash_table/static_set/insert_bench.cu
+++ benchmarks/hash_table/static_set/insert_bench.cu
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+#include <defaults.hpp>
 #include <key_generator.hpp>
 
 #include <cuco/static_set.cuh>
@@ -21,61 +22,57 @@
 #include <nvbench/nvbench.cuh>
 
 #include <thrust/device_vector.h>
+#include <thrust/execution_policy.h>
 
-#include <vector>
+using namespace cuco::benchmark;
+using namespace cuco::benchmark::defaults;
 
 /**
- * @brief A benchmark evaluating multi-value `insert` performance:
- * - Total number of insertions: 100'000'000
+ * @brief A benchmark evaluating `insert` performance:
  */
-template <typename Key, dist_type Dist, nvbench::int32_t Multiplicity>
-void nvbench_static_set_insert(
-  nvbench::state& state,
-  nvbench::type_list<Key, nvbench::enum_type<Dist>, nvbench::enum_type<Multiplicity>>)
+template <typename Key, typename Dist>
+void static_set_insert(nvbench::state& state, nvbench::type_list<Key, Dist>)
 {
   auto const num_keys  = state.get_int64("NumInputs");
   auto const occupancy = state.get_float64("Occupancy");
 
   std::size_t const size = num_keys / occupancy;
 
-  std::vector<Key> h_keys(num_keys);
+  thrust::device_vector<Key> keys(num_keys);
 
-  generate_keys<Dist, Multiplicity, Key>(h_keys.begin(), h_keys.end());
-  thrust::device_vector<Key> d_keys(h_keys);
+  key_generator gen;
+  gen.generate<Dist>(state, thrust::device, keys.begin(), keys.end());
 
   state.add_element_count(num_keys, "NumKeys");
-
+  state.set_global_memory_rw_bytes(num_keys * sizeof(Key));
   state.exec(nvbench::exec_tag::sync | nvbench::exec_tag::timer,
              [&](nvbench::launch& launch, auto& timer) {
                cuco::experimental::static_set<Key> myset{size, cuco::empty_key<Key>{-1}};
 
                // Use timers to explicitly mark the target region
                timer.start();
-               myset.insert(d_keys.begin(), d_keys.end(), launch.get_stream());
+               myset.insert(keys.begin(), keys.end(), launch.get_stream());
                timer.stop();
              });
 }
 
-using key_type = nvbench::type_list<nvbench::int32_t, nvbench::int64_t>;
-using d_type =
-  nvbench::enum_type_list<dist_type::GAUSSIAN, dist_type::GEOMETRIC, dist_type::UNIFORM>;
+NVBENCH_BENCH_TYPES(static_set_insert,
+                    NVBENCH_TYPE_AXES(KEY_TYPE_RANGE, nvbench::type_list<dist_type::uniform>))
+  .set_name("static_set_insert_uniform_multiplicity")
+  .set_type_axes_names({"Key", "Distribution"})
+  .set_max_noise(MAX_NOISE)  // Custom noise: 3%. By default: 0.5%.
+  .add_int64_axis("Multiplicity", MULTIPLICITY_RANGE);
 
-using multiplicity = nvbench::enum_type_list<1, 2, 4, 8, 16, 32, 64, 128, 256>;
+NVBENCH_BENCH_TYPES(static_set_insert,
+                    NVBENCH_TYPE_AXES(KEY_TYPE_RANGE, nvbench::type_list<dist_type::unique>))
+  .set_name("static_set_insert_unique_occupancy")
+  .set_type_axes_names({"Key", "Distribution"})
+  .set_max_noise(MAX_NOISE)  // Custom noise: 3%. By default: 0.5%.
+  .add_float64_axis("Occupancy", OCCUPANCY_RANGE);
 
-NVBENCH_BENCH_TYPES(nvbench_static_set_insert,
-                    NVBENCH_TYPE_AXES(key_type,
-                                      nvbench::enum_type_list<dist_type::UNIFORM>,
-                                      multiplicity))
-  .set_name("staic_set_insert_uniform_multiplicity")
-  .set_type_axes_names({"Key", "Distribution", "Multiplicity"})
-  .set_max_noise(3)                            // Custom noise: 3%. By default: 0.5%.
-  .add_int64_axis("NumInputs", {100'000'000})  // Total number of key/value pairs: 100'000'000
-  .add_float64_axis("Occupancy", {0.5});
-
-NVBENCH_BENCH_TYPES(nvbench_static_set_insert,
-                    NVBENCH_TYPE_AXES(key_type, d_type, nvbench::enum_type_list<1>))
-  .set_name("staic_set_insert_occupancy")
-  .set_type_axes_names({"Key", "Distribution", "Multiplicity"})
-  .set_max_noise(3)                            // Custom noise: 3%. By default: 0.5%.
-  .add_int64_axis("NumInputs", {100'000'000})  // Total number of key/value pairs: 100'000'000
-  .add_float64_axis("Occupancy", nvbench::range(0.1, 0.9, 0.1));
+NVBENCH_BENCH_TYPES(static_set_insert,
+                    NVBENCH_TYPE_AXES(KEY_TYPE_RANGE, nvbench::type_list<dist_type::gaussian>))
+  .set_name("static_set_insert_gaussian")
+  .set_type_axes_names({"Key", "Distribution"})
+  .set_max_noise(MAX_NOISE)  // Custom noise: 3%. By default: 0.5%.
+  .add_float64_axis("Skew", SKEW_RANGE);
