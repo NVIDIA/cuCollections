e8f40f2533682a355e7c30926d002e261fe07fb0
Yunsong Wang <yunsongw@nvidia.com>
2023-05-01 15:35:29 -0700

    More test cleanups

--- tests/static_set/retrieve_all_test.cu
+++ tests/static_set/retrieve_all_test.cu
@@ -20,12 +20,10 @@
 
 #include <thrust/device_vector.h>
 #include <thrust/distance.h>
-#include <thrust/execution_policy.h>
 #include <thrust/functional.h>
 #include <thrust/iterator/counting_iterator.h>
 #include <thrust/sequence.h>
 #include <thrust/sort.h>
-#include <thrust/transform.h>
 
 #include <catch2/catch_template_test_macros.hpp>
 
@@ -35,27 +33,25 @@ __inline__ void test_unique_sequence(Set& set, std::size_t num_keys)
   using Key = typename Set::key_type;
 
   thrust::device_vector<Key> d_keys(num_keys);
-
-  thrust::sequence(thrust::device, d_keys.begin(), d_keys.end());
-
-  auto key_begin = d_keys.begin();
+  thrust::sequence(d_keys.begin(), d_keys.end());
+  auto keys_begin = d_keys.begin();
 
   SECTION("Non-inserted keys should not be contained.")
   {
     REQUIRE(set.size() == 0);
 
-    auto key_end = set.retrieve_all(key_begin);
-    REQUIRE(std::distance(key_begin, key_end) == 0);
+    auto keys_end = set.retrieve_all(keys_begin);
+    REQUIRE(std::distance(keys_begin, keys_end) == 0);
   }
 
-  set.insert(key_begin, key_begin + num_keys);
+  set.insert(keys_begin, keys_begin + num_keys);
   REQUIRE(set.size() == num_keys);
 
   SECTION("All inserted key/value pairs should be contained.")
   {
     thrust::device_vector<Key> d_res(num_keys);
     auto d_res_end = set.retrieve_all(d_res.begin());
-    thrust::sort(thrust::device, d_res.begin(), d_res_end);
+    thrust::sort(d_res.begin(), d_res_end);
     REQUIRE(cuco::test::equal(
       d_res.begin(), d_res_end, thrust::counting_iterator<Key>(0), thrust::equal_to<Key>{}));
   }
@@ -79,38 +75,23 @@ TEMPLATE_TEST_CASE_SIG(
                                              : 422  // 211 x 2 x 1
     ;
 
-  using extent_type    = cuco::experimental::extent<std::size_t>;
-  using allocator_type = cuco::cuda_allocator<std::byte>;
-  using storage_type   = cuco::experimental::aow_storage<1>;
+  using probe =
+    std::conditional_t<Probe == cuco::test::probe_sequence::linear_probing,
+                       cuco::experimental::linear_probing<CGSize, cuco::murmurhash3_32<Key>>,
+                       cuco::experimental::double_hashing<CGSize,
+                                                          cuco::murmurhash3_32<Key>,
+                                                          cuco::murmurhash3_32<Key>>>;
 
-  if constexpr (Probe == cuco::test::probe_sequence::linear_probing) {
-    using probe = cuco::experimental::linear_probing<CGSize, cuco::murmurhash3_32<Key>>;
-    auto set    = cuco::experimental::static_set<Key,
-                                              extent_type,
-                                              cuda::thread_scope_device,
-                                              thrust::equal_to<Key>,
-                                              probe,
-                                              allocator_type,
-                                              storage_type>{num_keys, cuco::empty_key<Key>{-1}};
+  auto set = cuco::experimental::static_set<Key,
+                                            cuco::experimental::extent<std::size_t>,
+                                            cuda::thread_scope_device,
+                                            thrust::equal_to<Key>,
+                                            probe,
+                                            cuco::cuda_allocator<std::byte>,
+                                            cuco::experimental::aow_storage<1>>{
+    num_keys, cuco::empty_key<Key>{-1}};
 
-    REQUIRE(set.capacity() == gold_capacity);
+  REQUIRE(set.capacity() == gold_capacity);
 
-    test_unique_sequence(set, num_keys);
-  }
-
-  if constexpr (Probe == cuco::test::probe_sequence::double_hashing) {
-    using probe = cuco::experimental::
-      double_hashing<CGSize, cuco::murmurhash3_32<Key>, cuco::murmurhash3_32<Key>>;
-    auto set = cuco::experimental::static_set<Key,
-                                              extent_type,
-                                              cuda::thread_scope_device,
-                                              thrust::equal_to<Key>,
-                                              probe,
-                                              allocator_type,
-                                              storage_type>{num_keys, cuco::empty_key<Key>{-1}};
-
-    REQUIRE(set.capacity() == gold_capacity);
-
-    test_unique_sequence(set, num_keys);
-  }
+  test_unique_sequence(set, num_keys);
 }
