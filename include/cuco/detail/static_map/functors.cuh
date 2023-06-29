806aa8051ba933c758636586e2c34487282465f0
Yunsong Wang <yunsongw@nvidia.com>
2023-06-29 16:21:37 -0700

    Modify `experimental::static_map::retrieve_all` API (#325)
    This PR modifies the public `experimental::static_map::retrieve_all` API
    to take a key iterator and a value iterator separately.
    
    ---------
    
    Co-authored-by: Daniel Jünger <2955913+sleeepyjack@users.noreply.github.com>

--- include/cuco/detail/static_map/functors.cuh
+++ include/cuco/detail/static_map/functors.cuh
@@ -17,17 +17,51 @@
 
 #include <cuco/detail/bitwise_compare.cuh>
 
+#include <thrust/tuple.h>
+
 namespace cuco {
 namespace experimental {
 namespace static_map_ns {
 namespace detail {
 
 /**
+ * @brief Device functor returning the content of the slot indexed by `idx`.
+ *
+ * @tparam StorageRef Storage ref type
+ */
+template <typename StorageRef>
+struct get_slot {
+  StorageRef storage_;  ///< Storage ref
+
+  /**
+   * @brief Constructs `get_slot` functor with the given storage ref.
+   *
+   * @param s Input storage ref
+   */
+  explicit constexpr get_slot(StorageRef s) noexcept : storage_{s} {}
+
+  /**
+   * @brief Accesses the slot content with the given index.
+   *
+   * @param idx The slot index
+   * @return The slot content
+   */
+  __device__ constexpr auto operator()(typename StorageRef::size_type idx) const noexcept
+  {
+    auto const window_idx      = idx / StorageRef::window_size;
+    auto const intra_idx       = idx % StorageRef::window_size;
+    auto const [first, second] = storage_[window_idx][intra_idx];
+    return thrust::make_tuple(first, second);
+  }
+};
+
+/**
  * @brief Device functor returning whether the input slot indexed by `idx` is filled.
  *
- * @tparam T The slot content type
+ * @tparam T The slot key type
+ * @tparam U The slot value type
  */
-template <typename T>
+template <typename T, typename U>
 struct slot_is_filled {
   T empty_sentinel_;  ///< The value of the empty key sentinel
 
@@ -44,10 +78,23 @@ struct slot_is_filled {
    * @tparam U Slot content type
    *
    * @param slot The slot
+   *
    * @return `true` if slot is filled
    */
-  template <typename U>
-  __device__ constexpr bool operator()(U const& slot) const noexcept
+  template <typename Slot>
+  __device__ constexpr bool operator()(Slot const& slot) const noexcept
+  {
+    return not cuco::detail::bitwise_compare(empty_sentinel_, thrust::get<0>(slot));
+  }
+
+  /**
+   * @brief Indicates if the target slot `slot` is filled.
+   *
+   * @param slot The slot
+   *
+   * @return `true` if slot is filled
+   */
+  __device__ constexpr bool operator()(cuco::pair<T, U> const& slot) const noexcept
   {
     return not cuco::detail::bitwise_compare(empty_sentinel_, slot.first);
   }
