6f2b0b88641d0057a31f5559432a6ba8273c125a
Anurag Mukkara <amukkara@nvidia.com>
2023-10-13 07:20:03 +0000

    Use cuco key generators

--- tests/trie/trie_utils.hpp
+++ tests/trie/trie_utils.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <chrono>
+#include <cuco/utility/key_generator.hpp>
 #include <fstream>
 #include <sstream>
 #include <thrust/host_vector.h>
@@ -12,21 +13,27 @@ struct valid_key {
 };
 
 template <typename LabelType>
-void generate_labels(thrust::host_vector<LabelType>& keys,
+void generate_labels(thrust::host_vector<LabelType>& labels,
                      thrust::host_vector<size_t>& offsets,
                      size_t num_keys,
                      size_t max_key_length)
 {
-  for (size_t key_id = 0; key_id < num_keys; key_id++) {
-    size_t cur_key_length = 1 + (std::rand() % max_key_length);
-    offsets.push_back(cur_key_length);
-    for (size_t pos = 0; pos < cur_key_length; pos++) {
-      keys.push_back(std::rand() % 100000);
-    }
+  cuco::utility::key_generator gen;
+
+  cuco::utility::distribution::unique lengths_dist;
+  offsets.resize(num_keys);
+  gen.generate(lengths_dist, offsets.begin(), offsets.end());
+
+  for (auto& offset : offsets) {
+    offset = 1 + (offset % max_key_length);
   }
 
-  offsets.push_back(0);  // Extend size by 1 for subsequent scan
-  thrust::exclusive_scan(offsets.begin(), offsets.end(), offsets.begin());  // in-place scan
+  offsets.push_back(0);
+  thrust::exclusive_scan(offsets.begin(), offsets.end(), offsets.begin());
+
+  cuco::utility::distribution::gaussian labels_dist{0.5};
+  labels.resize(offsets.back());
+  gen.generate(labels_dist, labels.begin(), labels.end());
 }
 
 template <typename LabelType>
@@ -99,21 +106,4 @@ inline std::vector<std::vector<LabelType>> read_keys(const char* filename, size_
     keys.push_back(line);
   }
   return generate_split_keys<LabelType>(keys);
-}
-
-inline std::chrono::high_resolution_clock::time_point current_time()
-{
-  return std::chrono::high_resolution_clock::now();
-}
-inline size_t elapsed_seconds(std::chrono::high_resolution_clock::time_point begin)
-{
-  return std::chrono::duration_cast<std::chrono::seconds>(current_time() - begin).count();
-}
-inline size_t elapsed_milliseconds(std::chrono::high_resolution_clock::time_point begin)
-{
-  return std::chrono::duration_cast<std::chrono::milliseconds>(current_time() - begin).count();
-}
-inline size_t elapsed_microseconds(std::chrono::high_resolution_clock::time_point begin)
-{
-  return std::chrono::duration_cast<std::chrono::microseconds>(current_time() - begin).count();
 }
