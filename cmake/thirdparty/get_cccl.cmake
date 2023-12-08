8bed0e2f4b13a7b1cfc77dd7b32d03525ae3484e
Bradley Dice <bdice@bradleydice.com>
2023-12-08 09:22:56 -0800

    Apply suggestions from code review
    Co-authored-by: Robert Maynard <robertjmaynard@gmail.com>

--- cmake/thirdparty/get_cccl.cmake
+++ cmake/thirdparty/get_cccl.cmake
@@ -15,7 +15,7 @@
 # Use CPM to find or clone CCCL
 function(find_and_configure_cccl)
     include(${rapids-cmake-dir}/cpm/cccl.cmake)
-    rapids_cpm_cccl(BUILD_EXPORT_SET cuco-exports)
+    rapids_cpm_cccl(INSTALL_EXPORT_SET cuco-exports BUILD_EXPORT_SET cuco-exports)
 endfunction()
 
 find_and_configure_cccl()
