fbc559e9f2a854f50cafad33b7e520f037f86a80
Yunsong Wang <yunsongw@nvidia.com>
2024-01-03 18:28:00 -0800

    Remove the redundant thrust pair like trait

--- include/cuco/detail/traits.hpp
+++ include/cuco/detail/traits.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, NVIDIA CORPORATION.
+ * Copyright (c) 2023-2024, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,10 +16,10 @@
 #pragma once
 
 #include <thrust/device_reference.h>
-#include <thrust/tuple.h>
 
 #include <cuda/std/tuple>
 #include <cuda/std/type_traits>
+
 #include <tuple>
 
 namespace cuco::detail {
@@ -35,10 +35,10 @@ struct is_std_pair_like<T,
       conditional_t<std::tuple_size<T>::value == 2, cuda::std::true_type, cuda::std::false_type> {};
 
 template <typename T, typename = void>
-struct is_cuda_std_pair_like : cuda::std::false_type {};
+struct is_cuda_std_pair_like_impl : cuda::std::false_type {};
 
 template <typename T>
-struct is_cuda_std_pair_like<
+struct is_cuda_std_pair_like_impl<
   T,
   cuda::std::void_t<decltype(cuda::std::get<0>(cuda::std::declval<T>())),
                     decltype(cuda::std::get<1>(cuda::std::declval<T>()))>>
@@ -46,21 +46,9 @@ struct is_cuda_std_pair_like<
                              cuda::std::true_type,
                              cuda::std::false_type> {};
 
-template <typename T, typename = void>
-struct is_thrust_pair_like_impl : cuda::std::false_type {};
-
 template <typename T>
-struct is_thrust_pair_like_impl<
-  T,
-  cuda::std::void_t<decltype(thrust::get<0>(cuda::std::declval<T>())),
-                    decltype(thrust::get<1>(cuda::std::declval<T>()))>>
-  : cuda::std::conditional_t<thrust::tuple_size<T>::value == 2,
-                             cuda::std::true_type,
-                             cuda::std::false_type> {};
-
-template <typename T>
-struct is_thrust_pair_like
-  : is_thrust_pair_like_impl<cuda::std::remove_reference_t<decltype(thrust::raw_reference_cast(
+struct is_cuda_std_pair_like
+  : is_cuda_std_pair_like_impl<cuda::std::remove_reference_t<decltype(thrust::raw_reference_cast(
       cuda::std::declval<T>()))>> {};
 
 }  // namespace cuco::detail
