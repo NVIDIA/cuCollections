a992e356c30698224ee89c2e29029425db72825b
Yunsong Wang <yunsongw@nvidia.com>
2024-01-26 08:28:29 -0800

    Bump clang format version (#431)

--- include/cuco/static_map.cuh
+++ include/cuco/static_map.cuh
@@ -138,7 +138,7 @@ class static_map {
                                         storage_ref_type,
                                         Operators...>;  ///< Non-owning container ref type
 
-  static_map(static_map const&) = delete;
+  static_map(static_map const&)            = delete;
   static_map& operator=(static_map const&) = delete;
 
   static_map(static_map&&) = default;  ///< Move constructor
@@ -866,7 +866,7 @@ class static_map {
   static_map(static_map&&)      = delete;
 
   static_map& operator=(static_map const&) = delete;
-  static_map& operator=(static_map&&) = delete;
+  static_map& operator=(static_map&&)      = delete;
 
   /**
    * @brief Indicates if concurrent insert/find is supported for the key/value types.
