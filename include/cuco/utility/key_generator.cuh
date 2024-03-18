70428f78bb2f0040d1a780dd3145b8282abdebfa
Yunsong Wang <yunsongw@nvidia.com>
2024-03-18 16:35:39 -0700

    Clean up pair utilities

--- include/cuco/utility/key_generator.cuh
+++ include/cuco/utility/key_generator.cuh
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2023, NVIDIA CORPORATION.
+ * Copyright (c) 2021-2024, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@
 #pragma once
 
 #include <cuco/detail/error.hpp>
+#include <cuco/detail/pair/helpers.cuh>
 #include <cuco/detail/utils.cuh>
 
 #include <thrust/execution_policy.h>
