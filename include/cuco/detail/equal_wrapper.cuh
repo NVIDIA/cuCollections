8d5f8b1b3a022a6e8c1eb8b44597d3b963991620
Yunsong Wang <yunsongw@nvidia.com>
2024-05-20 14:49:31 -0700

    Move caveat from docs to comments

--- include/cuco/detail/equal_wrapper.cuh
+++ include/cuco/detail/equal_wrapper.cuh
@@ -24,9 +24,8 @@ namespace detail {
 
 /**
  * @brief Enum of equality comparison results
- *
- * @note ENUM VALUE MATTERS, DO NOT CHANGE
  */
+// ENUM VALUE MATTERS, DO NOT CHANGE
 enum class equal_result : int32_t { UNEQUAL = 0, EQUAL = 1, EMPTY = 2, AVAILABLE = 3 };
 
 enum class is_insert : bool { YES, NO };
