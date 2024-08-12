f251f6fffd5c2aaa61a53fab23bc850896f59cc6
Danial Javady <danialjavady96@gmail.com>
2024-08-12 15:33:29 -0400

    uncomment tests

--- tests/dynamic_map/erase_test.cu
+++ tests/dynamic_map/erase_test.cu
@@ -39,54 +39,54 @@ TEMPLATE_TEST_CASE_SIG("erase key",
                                     cuco::empty_value<Value>{-1},
                                     cuco::erased_key<Key>{-2}};
 
-  // SECTION("Check single submap insert/erase")
-  // {
-  //   thrust::device_vector<Key> d_keys(num_keys);
-  //   thrust::device_vector<Value> d_values(num_keys);
-  //   thrust::device_vector<bool> d_keys_exist(num_keys);
+  SECTION("Check single submap insert/erase")
+  {
+    thrust::device_vector<Key> d_keys(num_keys);
+    thrust::device_vector<Value> d_values(num_keys);
+    thrust::device_vector<bool> d_keys_exist(num_keys);
 
-  //   thrust::sequence(thrust::device, d_keys.begin(), d_keys.end(), 1);
-  //   thrust::sequence(thrust::device, d_values.begin(), d_values.end(), 1);
+    thrust::sequence(thrust::device, d_keys.begin(), d_keys.end(), 1);
+    thrust::sequence(thrust::device, d_values.begin(), d_values.end(), 1);
 
-  //   auto pairs_begin =
-  //     thrust::make_zip_iterator(thrust::make_tuple(d_keys.begin(), d_values.begin()));
+    auto pairs_begin =
+      thrust::make_zip_iterator(thrust::make_tuple(d_keys.begin(), d_values.begin()));
 
-  //   map.insert(pairs_begin, pairs_begin + num_keys);
+    map.insert(pairs_begin, pairs_begin + num_keys);
 
-  //   REQUIRE(map.get_size() == num_keys);
+    REQUIRE(map.get_size() == num_keys);
 
-  //   map.erase(d_keys.begin(), d_keys.end());
+    map.erase(d_keys.begin(), d_keys.end());
 
-  //   // delete decreases count correctly
-  //   REQUIRE(map.get_size() == 0);
+    // delete decreases count correctly
+    REQUIRE(map.get_size() == 0);
 
-  //   map.contains(d_keys.begin(), d_keys.end(), d_keys_exist.begin());
+    map.contains(d_keys.begin(), d_keys.end(), d_keys_exist.begin());
 
-  //   // keys were actaully deleted
-  //   REQUIRE(cuco::test::none_of(d_keys_exist.begin(), d_keys_exist.end(), thrust::identity{}));
+    // keys were actaully deleted
+    REQUIRE(cuco::test::none_of(d_keys_exist.begin(), d_keys_exist.end(), thrust::identity{}));
 
-  //   // ensures that map is reusing deleted slots
-  //   map.insert(pairs_begin, pairs_begin + num_keys);
+    // ensures that map is reusing deleted slots
+    map.insert(pairs_begin, pairs_begin + num_keys);
 
-  //   REQUIRE(map.get_size() == num_keys);
+    REQUIRE(map.get_size() == num_keys);
 
-  //   map.contains(d_keys.begin(), d_keys.end(), d_keys_exist.begin());
+    map.contains(d_keys.begin(), d_keys.end(), d_keys_exist.begin());
 
-  //   REQUIRE(cuco::test::all_of(d_keys_exist.begin(), d_keys_exist.end(), thrust::identity{}));
+    REQUIRE(cuco::test::all_of(d_keys_exist.begin(), d_keys_exist.end(), thrust::identity{}));
 
-  //   // erase can act selectively
-  //   map.erase(d_keys.begin(), d_keys.begin() + num_keys / 2);
-  //   map.contains(d_keys.begin(), d_keys.end(), d_keys_exist.begin());
+    // erase can act selectively
+    map.erase(d_keys.begin(), d_keys.begin() + num_keys / 2);
+    map.contains(d_keys.begin(), d_keys.end(), d_keys_exist.begin());
 
-  //   REQUIRE(cuco::test::none_of(
-  //     d_keys_exist.begin(), d_keys_exist.begin() + num_keys / 2, thrust::identity{}));
+    REQUIRE(cuco::test::none_of(
+      d_keys_exist.begin(), d_keys_exist.begin() + num_keys / 2, thrust::identity{}));
 
-  //   REQUIRE(cuco::test::all_of(
-  //     d_keys_exist.begin() + num_keys / 2, d_keys_exist.end(), thrust::identity{}));
+    REQUIRE(cuco::test::all_of(
+      d_keys_exist.begin() + num_keys / 2, d_keys_exist.end(), thrust::identity{}));
 
-  //   // clear map
-  //   map.erase(d_keys.begin() + num_keys / 2, d_keys.end());
-  // }
+    // clear map
+    map.erase(d_keys.begin() + num_keys / 2, d_keys.end());
+  }
 
   SECTION("Check multiple submaps insert/erase")
   {
