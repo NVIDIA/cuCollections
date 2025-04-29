60631c52bac360bb4ba39012e25f6310864a4a8e
Yunsong Wang <yunsongw@nvidia.com>
2025-04-29 14:21:19 -0700

    Cleanups

--- tests/static_set/spark_test.cu
+++ tests/static_set/spark_test.cu
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022-2025, NVIDIA CORPORATION.
+ * Copyright (c) 2025, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,19 +14,14 @@
  * limitations under the License.
  */
 
-#include <test_utils.hpp>
-
 #include <cuco/static_set.cuh>
 
 #include <cuda/functional>
 #include <thrust/device_vector.h>
-#include <thrust/distance.h>
-#include <thrust/iterator/constant_iterator.h>
 #include <thrust/iterator/counting_iterator.h>
-#include <thrust/sort.h>
-#include <thrust/transform.h>
+#include <thrust/iterator/transform_iterator.h>
 
-#include <catch2/catch_template_test_macros.hpp>
+#include <catch2/catch_test_macros.hpp>
 
 using T    = int32_t;
 using Hash = uint32_t;
