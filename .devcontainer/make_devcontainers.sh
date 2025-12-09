a0c2812ee9df49ff18a214cf120f80c025b419ce
Yunsong Wang <yunsongw@nvidia.com>
2025-12-09 14:32:32 -0800

    Updates

--- .devcontainer/make_devcontainers.sh
+++ .devcontainer/make_devcontainers.sh
@@ -45,7 +45,12 @@ update_devcontainer() {
     local devcontainer_version="$8"
 
     local IMAGE_ROOT="rapidsai/devcontainers:${devcontainer_version}-cpp-"
-    local image="${IMAGE_ROOT}${compiler_name}${compiler_version}-cuda${cuda_version}-${os}"
+    # Add 'ext' suffix for CUDA 13.0 images
+    local cuda_suffix=""
+    if [[ "$cuda_version" == "13.0" ]]; then
+        cuda_suffix="ext"
+    fi
+    local image="${IMAGE_ROOT}${compiler_name}${compiler_version}-cuda${cuda_version}${cuda_suffix}-${os}"
 
     jq --arg image "$image" --arg name "$name" \
        --arg cuda_version "$cuda_version" --arg compiler_name "$compiler_name" \
