e866d9d532d917011b60f5b46b72ac7c464dbf77
Yunsong Wang <yunsongw@nvidia.com>
2026-07-15 23:13:10 +0000

    Address SPDX migration review feedback

--- tests/static_map/insert_and_find_test.cu
+++ tests/static_map/insert_and_find_test.cu
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, Jonas Hahnfeld, CERN.
+ * SPDX-FileCopyrightText: Copyright (c) 2022, Jonas Hahnfeld, CERN.
  * SPDX-FileCopyrightText: Copyright (c) 2022-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
