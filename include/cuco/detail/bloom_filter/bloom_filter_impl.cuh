c761cf827f5d30d3c5555b1975cac1fa582b6d6f
Yunsong Wang <yunsongw@nvidia.com>
2026-08-12 23:45:43 +0000

    Check bloom filter word type is integral

--- include/cuco/detail/bloom_filter/bloom_filter_impl.cuh
+++ include/cuco/detail/bloom_filter/bloom_filter_impl.cuh
@@ -70,14 +70,7 @@ class bloom_filter_impl {
 
   static_assert(cuda::std::has_single_bit(words_per_block) and words_per_block <= 32,
                 "Number of words per block must be a power-of-two and less than or equal to 32");
-  static_assert(
-    cuda::std::is_constructible_v<cuda::atomic_ref<word_type, Scope>, word_type&> &&
-      cuda::std::is_invocable_r_v<word_type,
-                                  decltype(&cuda::atomic_ref<word_type, Scope>::fetch_or),
-                                  cuda::atomic_ref<word_type, Scope>*,
-                                  word_type,
-                                  cuda::std::memory_order>,
-    "Invalid word type");
+  static_assert(cuda::std::is_integral_v<word_type>, "word_type must be an integral type");
 
   __host__ __device__ static constexpr size_t alignment() noexcept
   {
